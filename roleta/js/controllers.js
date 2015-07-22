b0c4cd36f8a5c322bea137e9cccffa403385b01d
Carlos Fernando Avila Gratz <cafe@q1software.com>
2015-07-21 22:13:14 -0300

    added bets

--- roleta/js/controllers.js
+++ roleta/js/controllers.js
@@ -4,8 +4,19 @@ var app = angular.module('Simulator', []);
 
 app.controller('MainController', function ($scope) {
 
+    $scope.numeroBolas = 10;
     $scope.report = {};
-    $scope.numeroBolas = 100;
+    $scope.bets = {
+        black: true,
+        red: false,
+        even: true,
+        odd: false,
+        zero: false
+    };
+
+    $scope.toggleBet = function (bet) {
+        this.bets[bet] = !this.bets[bet];
+    };
 
     $scope.runSimulation = function (loopcount) {
         var min = 0,
@@ -14,28 +25,47 @@ app.controller('MainController', function ($scope) {
             balls = {},
             chance = {},
             report = {},
+            bets = this.bets,
+            roundCost = _.filter(_.values(bets), function(bet) { return bet === true; }).length,
+            betsReport = { won: 0, lost: 0, tie: 0 },
+            money = 0,
+            roundEarnings,
             i, n, color, type;
-
-        // reset
 
         // run
         for  (i = 0; i < loopcount; i = i + 1) {
             n = Math.floor(Math.random() * (max - min) + min);
-            // console.log(n);
 
             balls[n] = balls[n] || 0;
             balls[n]++;
 
+            // money
+            roundEarnings = 0;
+            money -= roundCost;
+
             if (n === 0) {
                 chance.zero = chance.zero || 0;
                 chance.zero++;
+
+                if (bets.zero) {
+                    roundEarnings += 2;
+                }
             }
             else {
 
                 color = (_.indexOf(black, n) > -1) ? 'black' : 'red';
                 type  = n % 2 === 0  ? 'even'  : 'odd';
 
+                // bets
+                if (bets[color]) {
+                    roundEarnings += 2;
+                }
 
+                if (bets[type]) {
+                    roundEarnings += 2;
+                }
+
+                // stats
                 chance[type] = chance[type] || 0;
                 chance[color] = chance[color] || 0;
                 chance[color + '_' + type] = chance[color + '_' + type] || 0;
@@ -44,6 +74,20 @@ app.controller('MainController', function ($scope) {
                 chance[color]++;
                 chance[color + '_' + type]++;
             }
+
+            // money
+            money += roundEarnings;
+
+            if (roundEarnings > roundCost) {
+                betsReport.won++;
+            }
+            else if (roundEarnings < roundCost) {
+                betsReport.lost++;
+            }
+            else {
+                betsReport.tie++;
+            }
+
         }
 
         // build report
@@ -56,6 +100,8 @@ app.controller('MainController', function ($scope) {
             return a[0].localeCompare(b[0]);
         });
 
+        report.money = money;
+        report.bets = betsReport;
         report.total = loopcount;
         report.bestBall = report.balls[0];
         report.worstBall = _.last(report.balls);
