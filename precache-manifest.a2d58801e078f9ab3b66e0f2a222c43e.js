809344cf841aae53d45c479d5b76ef4a8e4e8631
Carlos F A Gratz <cafe01@gmail.com>
2020-07-26 18:36:35 -0300

    added florafi app

---
+++ precache-manifest.a2d58801e078f9ab3b66e0f2a222c43e.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e7ae71e10c988a4d6c",
+    "url": "/css/app.06835de6.css"
+  },
+  {
+    "revision": "38e7ae71e10c988a4d6c",
+    "url": "/css/app.06835de6.css.map"
+  },
+  {
+    "revision": "afe9060e45d7e5a5a266",
+    "url": "/css/chunk-vendors.25af80cf.css"
+  },
+  {
+    "revision": "afe9060e45d7e5a5a266",
+    "url": "/css/chunk-vendors.25af80cf.css.map"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/fonts/materialdesignicons-webfont.6a2ddad1.ttf"
+  },
+  {
+    "revision": "8a67040660444e8d8ee82a406d580d8f",
+    "url": "/fonts/materialdesignicons-webfont.8a670406.eot"
+  },
+  {
+    "revision": "c32505e8c654310ece1da4e1c10eeb57",
+    "url": "/fonts/materialdesignicons-webfont.c32505e8.woff"
+  },
+  {
+    "revision": "dc85ceeb0daba687e36d8dde4ed4d352",
+    "url": "/fonts/materialdesignicons-webfont.dc85ceeb.woff2"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "20655a1a049351c46cf244f52ef3524d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38e7ae71e10c988a4d6c",
+    "url": "/js/app.f4c13768.js"
+  },
+  {
+    "revision": "38e7ae71e10c988a4d6c",
+    "url": "/js/app.f4c13768.js.map"
+  },
+  {
+    "revision": "afe9060e45d7e5a5a266",
+    "url": "/js/chunk-vendors.d7a59e3c.js"
+  },
+  {
+    "revision": "afe9060e45d7e5a5a266",
+    "url": "/js/chunk-vendors.d7a59e3c.js.map"
+  },
+  {
+    "revision": "d72a5bde51b4d1ca4262d9289b7e7e0e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
